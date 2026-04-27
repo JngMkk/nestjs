2ad6815e057b3e481e7309c15b8d2fee05fd4a77
JngMkk <kjm0817ss@gmail.com>
2026-04-28 00:03:49 +0900

    ✨ feat: Swagger API 문서 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 /**
  * Nest Application 시작점
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // https://docs.nestjs.com/openapi/introduction
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('NestJS API description')
+    .setVersion('1.0')
+    .addTag('NestJS')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
