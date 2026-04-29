efd3096d47ce29d42cb23bfb80ec28016208bce3
JngMkk <kjm0817ss@gmail.com>
2026-04-30 00:29:27 +0900

    ♻️ refactor: AuthService 내부 메서드에 private 접근 제한 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,62 +21,6 @@ export class AuthService {
   ) {}
 
   /**
-   * 토큰 서명
-   * - sub: 사용자 ID
-   * - type: 토큰 타입 (TokenType)
-   * @param sub - 사용자 ID (sub)
-   * @param type - 토큰 타입 (TokenType)
-   * @returns - 토큰
-   */
-  signToken(sub: number, type: TokenType): string {
-    const payload = { sub, type };
-
-    const expiresIn =
-      type === TokenType.ACCESS
-        ? ACCESS_TOKEN_EXPIRATION
-        : REFRESH_TOKEN_EXPIRATION;
-
-    return this.jwtService.sign(payload, {
-      secret: process.env.TOKEN_SECRET,
-      expiresIn,
-    });
-  }
-
-  /**
-   * 토큰 발급
-   * @param id - 사용자 ID
-   * @returns - 토큰 정보
-   */
-  issueTokens(id: number): ReadTokenDto {
-    return {
-      accessToken: this.signToken(id, TokenType.ACCESS),
-      refreshToken: this.signToken(id, TokenType.REFRESH),
-    };
-  }
-
-  /**
-   * 이메일과 비밀번호를 검증
-   * @param email - 사용자 이메일
-   * @param password - 사용자 비밀번호
-   * @returns - 사용자 정보
-   */
-  async authenticate(email: string, password: string): Promise<UserEntity> {
-    const foundUser = await this.usersService.getUserByEmail(email);
-    if (!foundUser) {
-      throw new UnauthorizedException('사용자를 찾을 수 없습니다.');
-    }
-
-    const isPasswordValid = bcrypt.compareSync(password, foundUser.password);
-    if (!isPasswordValid) {
-      throw new UnauthorizedException(
-        '이메일 또는 비밀번호가 일치하지 않습니다.',
-      );
-    }
-
-    return foundUser;
-  }
-
-  /**
    * 로그인
    * @param user - 사용자 정보
    * @returns - 토큰 정보
@@ -104,5 +48,64 @@ export class AuthService {
     });
 
     return this.issueTokens(newUser.id);
+  }
+
+  /**
+   * 토큰 서명
+   * - sub: 사용자 ID
+   * - type: 토큰 타입 (TokenType)
+   * @param sub - 사용자 ID (sub)
+   * @param type - 토큰 타입 (TokenType)
+   * @returns - 토큰
+   */
+  private signToken(sub: number, type: TokenType): string {
+    const payload = { sub, type };
+
+    const expiresIn =
+      type === TokenType.ACCESS
+        ? ACCESS_TOKEN_EXPIRATION
+        : REFRESH_TOKEN_EXPIRATION;
+
+    return this.jwtService.sign(payload, {
+      secret: process.env.TOKEN_SECRET,
+      expiresIn,
+    });
+  }
+
+  /**
+   * 토큰 발급
+   * @param id - 사용자 ID
+   * @returns - 토큰 정보
+   */
+  private issueTokens(id: number): ReadTokenDto {
+    return {
+      accessToken: this.signToken(id, TokenType.ACCESS),
+      refreshToken: this.signToken(id, TokenType.REFRESH),
+    };
+  }
+
+  /**
+   * 이메일과 비밀번호를 검증
+   * @param email - 사용자 이메일
+   * @param password - 사용자 비밀번호
+   * @returns - 사용자 정보
+   */
+  private async authenticate(
+    email: string,
+    password: string,
+  ): Promise<UserEntity> {
+    const foundUser = await this.usersService.getUserByEmail(email);
+    if (!foundUser) {
+      throw new UnauthorizedException('사용자를 찾을 수 없습니다.');
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, foundUser.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException(
+        '이메일 또는 비밀번호가 일치하지 않습니다.',
+      );
+    }
+
+    return foundUser;
   }
 }
