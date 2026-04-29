01a6dfbe4b0e108515948e421b59403c72eb679c
JngMkk <kjm0817ss@gmail.com>
2026-04-29 10:42:54 +0900

    ✨ feat: users 모듈을 TypeORM Repository 기반으로 전환
    - TypeOrmModule.forFeature로 UserEntity 등록
    - UserRepository 주입하여 createUser, getUsers 서비스 메서드 구현
    - POST /users, GET /users 엔드포인트 추가
    - CreateUserDto 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserEntity } from './entities/users.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Get()
+  getUsers(): Promise<UserEntity[]> {
+    return this.usersService.getUsers();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserEntity } from './entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const user = this.userRepository.create({
+      nickname: createUserDto.nickname,
+      email: createUserDto.email,
+      password: createUserDto.password,
+    });
+
+    return await this.userRepository.save(user);
+  }
+
+  async getUsers(): Promise<UserEntity[]> {
+    return await this.userRepository.find();
+  }
+}
